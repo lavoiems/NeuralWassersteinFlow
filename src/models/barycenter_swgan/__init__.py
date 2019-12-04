929f7ec8ce0300ff01819227666ef7a897238086
Sam <samuel.lavoie.m@gmail.com>
2019-12-03 20:16:17 -0500

    Barycenter

--- src/models/barycenter_swgan/__init__.py
+++ src/models/barycenter_swgan/__init__.py
@@ -16,12 +16,14 @@ def parse_args(parser):
 
 def execute(args):
     print(args)
-    dataset1 = toy.png(args.train_batch_size, 'duck.png')
-    dataset2 = toy.png(args.train_batch_size, 'heart.png')
-    tdataset1 = toy.png(args.test_batch_size, 'duck.png')
-    tdataset2 = toy.png(args.test_batch_size, 'heart.png')
-    args.loaders1 = (dataset1, tdataset1)
-    args.loaders2 = (dataset2, tdataset2)
+    #dataset1 = toy.png(args.train_batch_size, 'duck.png')
+    #dataset2 = toy.png(args.train_batch_size, 'heart.png')
+    #tdataset1 = toy.png(args.test_batch_size, 'duck.png')
+    #tdataset2 = toy.png(args.test_batch_size, 'heart.png')
+    dataset1 = toy.spiral(1, args.train_batch_size, translationx=0, translationy=0)
+    dataset2 = toy.spiral(-1, args.train_batch_size, translationx=0, translationy=0)
+    args.loaders1 = (dataset1, dataset1)
+    args.loaders2 = (dataset2, dataset2)
     args.shape1 = 2
 
     train(args)
