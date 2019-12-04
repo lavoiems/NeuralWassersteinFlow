696a3aa2ccfecf2e93dbb87b9f3ca0aa99fcc0a2
Sam <samuel.lavoie.m@gmail.com>
2019-12-03 19:53:32 -0500

    Barycenter

--- src/models/barycenter_sink/model.py
+++ src/models/barycenter_sink/model.py
@@ -6,19 +6,19 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         x = [nn.Linear(o_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, h_dim),
-             nn.ELU(inplace=True),
+             nn.ReLU(inplace=True),
              nn.Linear(h_dim, o_dim)]
 
         self.x = nn.Sequential(*x)
