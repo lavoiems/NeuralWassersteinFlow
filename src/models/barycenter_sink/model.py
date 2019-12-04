da73e18d14c572d56f7b53d3f500d223382475cb
Sam <samuel.lavoie.m@gmail.com>
2019-12-03 19:49:02 -0500

    Barycenter

--- src/models/barycenter_sink/model.py
+++ src/models/barycenter_sink/model.py
@@ -9,6 +9,16 @@ class Generator(nn.Module):
              nn.ELU(inplace=True),
              nn.Linear(h_dim, h_dim),
              nn.ELU(inplace=True),
+             nn.Linear(h_dim, h_dim),
+             nn.ELU(inplace=True),
+             nn.Linear(h_dim, h_dim),
+             nn.ELU(inplace=True),
+             nn.Linear(h_dim, h_dim),
+             nn.ELU(inplace=True),
+             nn.Linear(h_dim, h_dim),
+             nn.ELU(inplace=True),
+             nn.Linear(h_dim, h_dim),
+             nn.ELU(inplace=True),
              nn.Linear(h_dim, o_dim)]
 
         self.x = nn.Sequential(*x)
