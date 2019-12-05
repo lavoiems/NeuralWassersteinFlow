f721271eedc49d72254c9096192be5a74b17d0be
Sam <samuel.lavoie.m@gmail.com>
2019-12-05 13:59:00 -0500

    Barycenter

--- src/models/swgan_toy/train.py
+++ src/models/swgan_toy/train.py
@@ -34,7 +34,7 @@ def transfer_loss(data, target, eps, lp, critic1, critic2, generator, device):
     H = torch.clamp(u_ + v_ - (torch.abs(target_ - data_)**lp).sum(2), 0)
     H = H/eps
     gen_ = gen.view(gen.shape[0], -1).unsqueeze(1)
-    loss = (torch.abs(target_ - gen_)**lp).sum(2)*H.detach()
+    loss = (torch.abs(target_ - gen_)**lp).sum(2)#*H.detach()
     return loss.mean()
 
 
