d9ee6056fbd294e9cd2f2fbf8248f3549b07e6e0
Sam <samuel.lavoie.m@gmail.com>
2019-12-05 13:21:01 -0500

    Barycenter

--- src/models/swgan_toy/train.py
+++ src/models/swgan_toy/train.py
@@ -18,7 +18,7 @@ def disc_loss_generation(data, target, eps, lp, critic1, critic2):
     data_ = data.view(data.shape[0], -1).unsqueeze(0)
     target_ = target.view(target.shape[0], -1).unsqueeze(1)
     p = (u_ + v_ - (torch.abs(target_ - data_)**lp).sum(2))
-    p.clamp_(0)
+    #p.clamp_(0)
     p = -(1/(4*eps))*p**2
     return u.mean(), v.mean(), p.mean()
 
