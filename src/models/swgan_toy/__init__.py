8ceff6a5a482f0e0ec84c56f547517d5a21d99b3
Sam <samuel.lavoie.m@gmail.com>
2019-12-05 16:07:04 -0500

    Barycenter

--- src/models/swgan_toy/__init__.py
+++ src/models/swgan_toy/__init__.py
@@ -16,8 +16,8 @@ def parse_args(parser):
 
 def execute(args):
     print(args)
-    dataset1 = toy.gaussian1(args.train_batch_size)
-    dataset2 = toy.gaussian2(args.train_batch_size)
+    dataset1 = toy.gaussian2(args.train_batch_size)
+    dataset2 = toy.mixture2(args.train_batch_size)
     #dataset1 = toy.spiral(1, args.train_batch_size, translationx=0, translationy=0)
     #dataset2 = toy.spiral(1, args.train_batch_size, translationx=0, translationy=0)
     args.loaders1 = (dataset1, dataset1)
