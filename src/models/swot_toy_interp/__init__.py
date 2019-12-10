46e2ab268e44b4385af0d8c3edeff85810823ba3
Sam <samuel.lavoie.m@gmail.com>
2019-12-10 17:39:09 -0500

    w2

--- src/models/swot_toy_interp/__init__.py
+++ src/models/swot_toy_interp/__init__.py
@@ -6,14 +6,11 @@ def parse_args(parser):
     parser.add_argument('--dataset1', type=str, default='gaussian')
     parser.add_argument('--h-dim', type=int, default=64)
     parser.add_argument('--lr', type=float, default=2e-4)
-    parser.add_argument('--beta1', type=float, default=0.5)
+    parser.add_argument('--beta1', type=float, default=0)
     parser.add_argument('--beta2', type=float, default=0.99)
     parser.add_argument('--d-updates', type=int, default=5)
     parser.add_argument('--lp', type=int, default=2)
-    parser.add_argument('--nt', type=int, default=2)
-    parser.add_argument('--p-exp', type=int, default=2)
     parser.add_argument('--eps', type=float, default=0.1)
-    parser.add_argument('--alpha', type=float, default=1)
 
 
 def execute(args):
