1d9dee0e18de45d2945e046975244413cf76cb71
Sam <samuel.lavoie.m@gmail.com>
2019-12-11 22:11:41 -0500

    w2

--- src/models/swot_interp/train.py
+++ src/models/swot_interp/train.py
@@ -122,6 +122,7 @@ def train(args):
             print(f'Critics-{i}')
             print('Iter: %s' % i, time.time() - t0)
             args.visualiser.plot(step=i, data=p.detach().cpu().numpy(), title=f'Penalty')
+            d_loss = (r_loss+g_loss).detach().cpu().numpy()
             args.visualiser.plot(step=i, data=d_loss, title=f'Critic loss Y')
             t0 = time.time()
     for i in range(iteration, args.iterations):
@@ -162,7 +163,6 @@ def train(args):
             batchy, titer2 = sample(titer2, test_loader2)
             datay = batchy[0].to(args.device)
             evaluate(args.visualiser, datax, datay, generator, 'x', args.device)
-            d_loss = (r_loss+g_loss).detach().cpu().numpy()
             args.visualiser.plot(step=i, data=t_lossx.detach().cpu().numpy(), title=f'Generator loss X')
             args.visualiser.plot(step=i, data=t_lossy.detach().cpu().numpy(), title=f'Generator loss Y')
             t0 = time.time()
