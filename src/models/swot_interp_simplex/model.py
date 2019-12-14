0e23d777addfcbba95e8ed9f509cf484ba62741f
Sam <samuel.lavoie.m@gmail.com>
2019-12-13 23:05:14 -0500

    w2

--- src/models/swot_interp_simplex/model.py
+++ src/models/swot_interp_simplex/model.py
@@ -21,18 +21,6 @@ class Critic(nn.Module):
         return self.x(x).squeeze()
 
 
-class Block(nn.Module):
-    def __init__(self, in_dim, out_dim, h_dim):
-        super(Block, self).__init__()
-        self.conv = nn.Conv2d(in_dim, out_dim, 3, 1, 1)
-        self.tcbn = TCBN(h_dim, out_dim)
-
-    def forward(self, x, t):
-        o = self.conv(x)
-        o = self.tcbn(o, t[0])
-        return F.relu(o)
-
-
 class Generator(nn.Module):
     def __init__(self, o_dim, h_dim, **kwargs):
         super(Generator, self).__init__()
@@ -66,36 +54,49 @@ class Generator(nn.Module):
         return self.out(o)
 
 
-class TCBN(nn.Module):
-    def __init__(self, n_hidden, num_features, eps=1e-5):
-        super(TCBN, self).__init__()
-        self.eps = eps
+#class TCBN(nn.Module):
+#    def __init__(self, n_hidden, num_features, eps=1e-5):
+#        super(TCBN, self).__init__()
+#        self.eps = eps
+#
+#        # MLP used to predict betas and gammas
+#        self.fc_gamma = nn.Sequential(
+#            nn.Linear(4, n_hidden),
+#            nn.ReLU(inplace=True),
+#            nn.Linear(n_hidden, num_features),
+#        )
+#
+#        self.fc_beta = nn.Sequential(
+#            nn.Linear(4, n_hidden),
+#            nn.ReLU(inplace=True),
+#            nn.Linear(n_hidden, num_features),
+#        )
+#        self.mean = torch.FloatTensor([0])
+#        self.var = torch.FloatTensor([1])
+#
+#        # Initialize weights using Xavier initialization and biases with constant value
+#        for m in self.modules():
+#            if isinstance(m, nn.Linear):
+#                nn.init.xavier_uniform(m.weight)
+#                nn.init.constant(m.bias, 0.1)
+#
+#    def forward(self, input, t):
+#        # Obtain delta values from MLP
+#        gamma = 1-self.fc_gamma(t)
+#        beta = self.fc_beta(t)
+#
+#        # Standard batch normalization
+#        return F.batch_norm(input, None, None, gamma, beta, True, 0, self.eps)
+#
+#class Block(nn.Module):
+#    def __init__(self, in_dim, out_dim, h_dim):
+#        super(Block, self).__init__()
+#        self.conv = nn.Conv2d(in_dim, out_dim, 3, 1, 1)
+#        self.tcbn = TCBN(h_dim, out_dim)
+#
+#    def forward(self, x, t):
+#        o = self.conv(x)
+#        o = self.tcbn(o, t[0])
+#        return F.relu(o)
 
-        # MLP used to predict betas and gammas
-        self.fc_gamma = nn.Sequential(
-            nn.Linear(4, n_hidden),
-            nn.ReLU(inplace=True),
-            nn.Linear(n_hidden, num_features),
-        )
 
-        self.fc_beta = nn.Sequential(
-            nn.Linear(4, n_hidden),
-            nn.ReLU(inplace=True),
-            nn.Linear(n_hidden, num_features),
-        )
-        self.mean = torch.FloatTensor([0])
-        self.var = torch.FloatTensor([1])
-
-        # Initialize weights using Xavier initialization and biases with constant value
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform(m.weight)
-                nn.init.constant(m.bias, 0.1)
-
-    def forward(self, input, t):
-        # Obtain delta values from MLP
-        gamma = 1-self.fc_gamma(t)
-        beta = self.fc_beta(t)
-
-        # Standard batch normalization
-        return F.batch_norm(input, None, None, gamma, beta, True, 0, self.eps)
