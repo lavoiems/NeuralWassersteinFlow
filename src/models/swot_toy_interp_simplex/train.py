5ecaaac9381a1cd089aa9a2f75867728e3e552a0
Sam <samuel.lavoie.m@gmail.com>
2020-04-10 14:35:25 -0400

    toy simplex

--- src/models/swot_toy_interp_simplex/train.py
+++ src/models/swot_toy_interp_simplex/train.py
@@ -237,7 +237,7 @@ def train(args):
             #optim_criticw2.step()
 
         optim_generator.zero_grad()
-        t_ = Dirichlet(torch.FloatTensor([1.,1.,1.,1.])).sample().to(args.device)
+        t_ = Dirichlet(torch.FloatTensor([1.,1.,1.])).sample().to(args.device)
         t = torch.stack([t_]*input_data.shape[0])
         tinputdata = torch.cat([input_data]*args.nt)
         tdata = torch.cat([data]*args.nt)
